623b80c3596938435bd0cd7a8238fe13041f7567
Alberto Hernández Pintor <alberto1280@upsa.es>
2021-02-14 17:09:25 +0100

    Initial commit

---
+++ FitBreakApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitBreakApp.swift
+//  FitBreak
+//
+//  Created by Alberto Hernández Pintor on 14/2/21.
+//
+
+import SwiftUI
+
+@main
+struct FitBreakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
